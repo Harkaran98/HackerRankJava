48974374d5b46f8fcf997acd2275e378d52d2b70
Harkaran98 <harkaransingh8991@gmail.com>
2020-12-01 12:46:59 -0800

    Problem - JavaCurrency

---
+++ src/JavaCurrency.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.text.*;
+
+public class JavaCurrency {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("US: " + n.format(payment));
+        System.out.println("India: Rs." + n.format(payment).substring(1));
+        n = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        System.out.println("China: " + n.format(payment));
+        n = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        System.out.println("France: " + n.format(payment));
+    }
+}
